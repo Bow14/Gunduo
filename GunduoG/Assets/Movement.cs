3425890de74e8513caced0fc8f14e1f041ab0878
Cameron burridge <cmburridge4022@gmail.com>
2020-02-24 19:05:41 -0700

    Added simple character movement

---
+++ GunduoG/Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed = 20f;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Moving around the Board
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(Vector3.left*moveSpeed*Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector3.right*moveSpeed*Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}	
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		}
+	}
+}
