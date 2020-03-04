13ba8b8e0f09554cbe6a71ce224cc59ab4d7b0b9
Cameron burridge <cmburridge4022@gmail.com>
2020-03-03 17:52:17 -0700

    Added shooting and target destroy brackeys tutorial

--- GunduoG/Assets/PlayerMovement.cs
+++ GunduoG/Assets/PlayerMovement.cs
@@ -29,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
 
 		if (isGrounded && velocity.y < 0)
 		{
-			velocity.y = -2f;
+			velocity.y = -8f;
 		}
 		
 		float x = Input.GetAxis("Horizontal");
